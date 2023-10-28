20834cb850ae3099ace8466706145a5871309262
FatihMuzaqi <145949071+FatihMuzaqi@users.noreply.github.com>
2023-10-28 20:47:03 +0700

    commit

---
+++ latihan18_kelipatan.java
@@ -0,0 +1,10 @@
+package tipedata;
+public class latihan18_kelipatan {
+    public static void main(String[] args){
+    System.out.println("Bilangan kelipatan 3 dan 5 dari 3 sampai 35:");
+        for (double i = 3.5; i <= 35; i+=3.5){
+            System.out.println(i);
+            
+        }
+    }
+}
